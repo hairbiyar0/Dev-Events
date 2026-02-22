5fede50bf8ff0acd7f7a73f28d68e35b6ce3e4f4
Shahzaib <hairbiyarbuzdar.com>
2026-02-22 16:29:28 +0500

    Fix: Remove mongoose.ts re-export and use direct import to resolve Vercel build error

--- my-app/database/index.ts
+++ my-app/database/index.ts
@@ -2,4 +2,4 @@ export { default as Event } from "./models/event.model";
 export type { IEvent } from "./models/event.model";
 export { default as Booking } from "./models/booking.model";
 export type { IBooking } from "./models/booking.model";
-export { default as connectToDatabase } from "./mongoose";
+export { default as connectToDatabase } from "@/lib/mongodb";
